44f3e154ba06d65e4894ad75bf69f8263f488c2f
WonyeolHEO <hyy96hyy@naver.com>
2024-06-04 16:45:08 +0900

    Project initial Setting

---
+++ MoneyCamera/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoneyCamera
+//
+//  Created by wonyoul heo on 6/4/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
