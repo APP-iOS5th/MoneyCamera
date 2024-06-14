da07fd23f764e0e31c3f7ad6af0f6826ba05d864
WonyeolHEO <hyy96hyy@naver.com>
2024-06-14 14:46:39 +0900

    appDelegate 내용 삭제

--- MoneyCamera/Controllers/AppDelegate.swift
+++ MoneyCamera/Controllers/AppDelegate.swift
@@ -12,11 +12,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        let window = UIWindow(frame: UIScreen.main.bounds)
-        let rootViewController = UINavigationController(rootViewController: RequestImageViewController())
-        window.rootViewController = rootViewController
-        window.makeKeyAndVisible()
-        self.window = window
         return true
     }
 
